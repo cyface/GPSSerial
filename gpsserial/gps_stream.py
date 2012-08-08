8e91708b2b370bf8c563d98f8129aa59eccd4d7e
Tim White <tim@cyface.com>
2012-08-07 21:44:32 -0600

    Added distance diff calculation.

--- gpsserial/gps_stream.py
+++ gpsserial/gps_stream.py
@@ -1,10 +1,15 @@
 """Pulls live Serial Stream"""
 
 import serial
+from geolocator import gislib
 
 ser = serial.Serial('/dev/tty.Qstarz818XT-SPPslave', 19200, timeout=1)
-for i in range(1,10):
+
+last_point = (0.0, 0.0)
+
+for i in range(1, 10):
     line = (ser.readline())  # read a '\n' terminated line
+    print line
     data = line.split(',')
     if len(data) == 15:
         lat = data[2]
@@ -12,4 +17,8 @@ for i in range(1,10):
         long = data[4]
         long_dir = data[5]
         print "lat: {0} {1} long: {2} {3}".format(lat, lat_dir, long, long_dir)
+        point = (float(lat), float(long) * -1.0)
+        print gislib.getDistance(point, last_point)
+        last_point = point
+
 ser.close()
